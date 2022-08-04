bd381ffd78e43ecbbb2aa4fd0857d6df47756175
ungmo2 <ungmo2@gmail.com>
2022-08-04 15:53:11 +0900

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const products = require('./api/products');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/products', products);
 
